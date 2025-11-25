fdbe3d99f430cba3ba0a48315680ff5fd5057c00
zura <zuramanagadze1@yahoo.com>
2025-11-25 12:13:11 +0400

    ui: added header

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,6 +3,7 @@ import Wrapper from "../../components/shared/Wrapper";
 import AggregationPeriod from "./components/AggregationPeriod";
 import CampaignMetrics from "./components/CampaignMetrics";
 import Metrics from "./components/Metrics";
+import Header from "./components/Header";
 
 export default function Dashboard() {
   const [searchParams] = useSearchParams();
@@ -11,6 +12,7 @@ export default function Dashboard() {
 
   return (
     <Wrapper className="min-h-screen bg-background space-y-8 py-12">
+      <Header />
       <div className="flex flex-col gap-4 sm:flex-row sm:gap-0  sm:items-center sm:justify-between">
         <AggregationPeriod />
         <Metrics />
