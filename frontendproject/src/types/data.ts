1858f134f4a6b9fa643fdf033bd838ea76fec440
Anri Ugrekhelidze <anriugrexelidze2006@gmail.com>
2025-11-27 01:44:10 +0400

    feat: updated data types for table

--- frontendproject/src/types/data.ts
+++ frontendproject/src/types/data.ts
@@ -11,6 +11,10 @@ export type AggregationLevel = "hourly" | "daily" | "weekly" | "monthly";
 export type AggregatedBucket = {
   key: string;
   label: string;
+  date: string;
+  campaignsActive: number;
+  totalImpressions: number;
   totalClicks: number;
+  totalRevenue: number;
 };
 
