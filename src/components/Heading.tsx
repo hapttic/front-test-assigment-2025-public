8999b7a83144921c11349c330e210429bbfc8da6
Daduka Khukhunashvili <daduka.xuxunashvili@gmail.com>
2025-11-30 19:14:52 +0100

    Add Hapttic Logo

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,12 @@
 
 function Heading() {
     return (
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-bold text-white tracking-tight">Campaign Dashboard</h1>
-          <h3 className="text-zinc-400 mt-1">Real-time insights into campaign metrics and revenue.</h3>
+        <div className="flex items-center">
+          <img src="/Logo.svg" alt="Logo" className="w-12 h-12 mr-4" />
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold text-white tracking-tight">Campaign Dashboard</h1>
+            <h3 className="text-zinc-400 mt-1">Real-time insights into campaign metrics and revenue.</h3>
+          </div>
         </div>
     );
 }
