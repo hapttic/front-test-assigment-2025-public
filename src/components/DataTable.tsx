8fc3c132e6c7394b67d48e3e193b81f1f88a0db5
tchkoidze <khvicha.tchkoidze@toedur.com>
2025-12-01 11:45:22 +0400

    style: update table scroll, and make table head sticky

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -2,34 +2,21 @@ import type { AggregatedSlot } from "../types";
 
 export default function DataTable({ rows }: { rows: AggregatedSlot[] }) {
   return (
-    <div className=" rounded-2xl overflow-hidden shadow-md">
-      {/* className="overflow-x-auto max-h-[500px] border border-gray-300 rounded-2xl" */}
-      <table className="min-w-full text-sm">
-        <thead>
-          <tr className="border-b bg-gray-50">
-            <th className="p-2">Date</th>
-            <th className="p-2">Campaigns Active</th>
-            <th className="p-2">Impressions</th>
-            <th className="p-2">Clicks</th>
-            <th className="p-2">Revenue</th>
-          </tr>
-        </thead>
-
-        {/* <tbody>
-          {rows.map((r, i) => (
-            <tr key={i} className="border-b hover:bg-gray-100">
-              <td className="p-2">{r.start.toISOString()}</td>
-              <td className="p-2">{r.campaignsActive}</td>
-              <td className="p-2">{r.impressions}</td>
-              <td className="p-2">{r.clicks}</td>
-              <td className="p-2">${r.revenue.toFixed(2)}</td>
+    <div className="border border-gray-300 rounded-2xl shadow-md overflow-hidden">
+      <div className="overflow-auto max-h-[500px]">
+        {/* className="overflow-x-auto max-h-[500px] border border-gray-300 rounded-2xl" border border-gray-300 rounded-2xl */}
+        {/* className=" rounded-2xl overflow-hidden shadow-md" */}
+        <table className="min-w-full text-sm table-fixed">
+          <thead className="bg-gray-50 sticky top-0 z-10">
+            <tr className="border-b">
+              <th className="p-2 text-left">Date</th>
+              <th className="p-2 text-left">Campaigns Active</th>
+              <th className="p-2 text-left">Impressions</th>
+              <th className="p-2 text-left">Clicks</th>
+              <th className="p-2 text-left">Revenue</th>
             </tr>
-          ))}
-        </tbody> */}
-      </table>
+          </thead>
 
-      <div className="max-h-[500px] overflow-auto">
-        <table className="min-w-full text-sm">
           <tbody>
             {rows.map((r, i) => (
               <tr key={i} className="border-b hover:bg-gray-100">
